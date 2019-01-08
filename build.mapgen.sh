97cdf1a65b7af7e1b63f001c2b95e192ff3287f5
Blayne Chard <blayne@chard.com>
2019-01-08 23:15:23 +1300

    Readd missing build script

---
+++ build.mapgen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir bin -p
+i686-w64-mingw32-g++ -o bin/pimap.exe -Wno-write-strings src/mapgen/json.c src/mapgen/map.c src/mapgen/offset.c src/mapgen/d2_client.c -static-libgcc -static-libstdc++
